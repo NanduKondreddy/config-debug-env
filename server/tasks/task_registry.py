6a531248daec4ac405c3645909fbd876334c72bb
Kondreddy Nanda Kumar Reddy <knkreddy2201@gmail.com>
2026-04-11 16:06:44 +0530

    Fix: limit task registry to 3 validated graded tasks (JSON, YAML, Dockerfile)

--- server/tasks/task_registry.py
+++ server/tasks/task_registry.py
@@ -1,14 +1,9 @@
 from typing import Callable, Dict, Tuple, List
 
 from server.tasks import task1_json, task2_yaml, task3_dockerfile
-from server.tasks import task4_compose, task5_k8s, task6_github_actions, task7_nginx
 from server.graders.json_grader import grade_task1
 from server.graders.yaml_grader import grade_task2
 from server.graders.dockerfile_grader import grade_task3
-from server.graders.compose_grader import grade_task4
-from server.graders.k8s_grader import grade_task5
-from server.graders.github_actions_grader import grade_task6
-from server.graders.nginx_grader import grade_task7
 
 
 class TaskInfo:
@@ -28,20 +23,12 @@ TASK_ORDER = [
     "task1_json",
     "task2_yaml",
     "task3_dockerfile",
-    "task4_compose",
-    "task5_k8s",
-    "task6_github_actions",
-    "task7_nginx",
 ]
 
 TASK_REGISTRY: Dict[str, TaskInfo] = {
     "task1_json": TaskInfo(task1_json, grade_task1),
     "task2_yaml": TaskInfo(task2_yaml, grade_task2),
     "task3_dockerfile": TaskInfo(task3_dockerfile, grade_task3),
-    "task4_compose": TaskInfo(task4_compose, grade_task4),
-    "task5_k8s": TaskInfo(task5_k8s, grade_task5),
-    "task6_github_actions": TaskInfo(task6_github_actions, grade_task6),
-    "task7_nginx": TaskInfo(task7_nginx, grade_task7),
 }
 
 
